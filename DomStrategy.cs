901d99e787a7d3c4d2ba26170b28f761995aec40
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-11-18 00:30:38 +0200

    Add some new files

---
+++ DomStrategy.cs
@@ -0,0 +1,38 @@
+﻿using Laba2;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace Laba2
+{
+    public class DomStrategy : ISearchStrategy
+    {
+        public List<Student> Search(SearchCriteria criteria, string xmlPath)
+        {
+            var results = new List<Student>();
+            var doc = new XmlDocument();
+            doc.Load(xmlPath);
+
+            string xPath = $"//Student[{criteria.Field}='{criteria.Value}']";
+
+            if (string.IsNullOrEmpty(criteria.Field) || string.IsNullOrEmpty(criteria.Value))
+            {
+                xPath = "//Student";
+            }
+
+            XmlNodeList nodes = doc.SelectNodes(xPath);
+
+            foreach (XmlNode node in nodes)
+            {
+                results.Add(new Student
+                {
+                    FullName = node["FullName"]?.InnerText,
+                    Faculty = node["Faculty"]?.InnerText,
+                    Course = node["Course"]?.InnerText,
+                    RoomNumber = node["RoomNumber"]?.InnerText,
+                    MonthlyFee = node["MonthlyFee"]?.InnerText
+                });
+            }
+            return results;
+        }
+    }
+}
---
+++ ISearchStrategy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Laba2 
+{
+    public class Student
+    {
+        public string FullName { get; set; }
+        public string Faculty { get; set; }
+        public string Course { get; set; }
+        public string RoomNumber { get; set; }
+        public string MonthlyFee { get; set; }
+
+        public override string ToString()
+        {
+            return $"ПІП: {FullName}\nФакультет: {Faculty}\nКурс: {Course}\nКімната: {RoomNumber}\nПлата: {MonthlyFee} грн\n";
+        }
+    }
+
+    public class SearchCriteria
+    {
+        public string Field { get; set; } 
+        public string Value { get; set; } 
+    }
+
+    public interface ISearchStrategy
+    {
+        List<Student> Search(SearchCriteria criteria, string xmlPath);
+    }
+}
